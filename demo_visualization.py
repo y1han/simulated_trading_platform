cac8d6c1a905f6c60f3ee554add2bfe6f0a53bd5
Yi Han <chris.yi.han@outlook.com>
2022-12-20 09:39:17 +0800

    Minor Adjustments

--- demo_visualization.py
+++ demo_visualization.py
@@ -3,11 +3,11 @@ from datetime import time
 from visualization import *
 
 
-sim = mkt.Simulator(code=688017)
+sim = mkt.Simulator(code=688017, date=20220704)
 
 sim.reset()
 current_time = time(hour=9, minute=15, second=0)
-end_time = time(hour=10, minute=50, second=0)
+end_time = time(hour=15, minute=00, second=0)
 
 while current_time < end_time:
     current_time = sim.next_step()
--- visualization/spread.py
+++ visualization/spread.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
+from datetime import time
 from market import OrderBook
+
+OPEN_TIME = time(9, 30, 0)
+CLOSE_TIME = time(14, 57, 0)
 
 
 def draw_spread(orderbook: OrderBook):
@@ -8,9 +12,12 @@ def draw_spread(orderbook: OrderBook):
     plt.show()
 
 
-def draw_historical_spread(orderbook: OrderBook):
+def draw_historical_spread(orderbook: OrderBook, ignore_auction=True):
     bid_p = orderbook.historical_orderbook[["time", "bid_p_1"]].set_index("time")
     ask_p = orderbook.historical_orderbook[["time", "ask_p_1"]].set_index("time")
+    if ignore_auction:
+        bid_p = bid_p.between_time(OPEN_TIME, CLOSE_TIME)
+        ask_p = ask_p.between_time(OPEN_TIME, CLOSE_TIME)
     plt.plot(bid_p)
     plt.plot(ask_p)
     plt.show()
