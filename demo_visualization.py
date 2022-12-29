d087d92a74f60e25a1398e2d2dc952809a01fbe3
Yi Han <chris.yi.han@outlook.com>
2022-12-29 10:45:23 +0800

    Structure adjustment

--- demo_visualization.py
+++ demo_visualization.py
@@ -1,21 +1,24 @@
+import pandas as pd
+
 import market as mkt
 from datetime import time
 from visualization import *
 
-sim = mkt.Simulator(code=688093, date=20220718)
-
+date = 20221209
+sim = mkt.Simulator(code=688363, date=date)
 sim.reset()
 current_time = time(hour=9, minute=15, second=0)
 end_time = time(hour=15, minute=00, second=0)
 
 while current_time < end_time:
     current_time = sim.next_step()
+    current_time = current_time.time()
 
 # draw_spread(sim.order_book, given_time=time(hour=9, minute=30, second=0))
 # draw_historical_spread(sim.order_book)
 # draw_corr_price_volume(sim.order_book, 3)
-draw_practical_corr_price_volume(sim.order_book, 1)
+# draw_practical_corr_price_volume(sim.order_book, 1)
 # draw_signature_plot_series(sim.order_book)
 # draw_cum_trading_volume(sim.order_book)
 # draw_order_book_depth_trend(sim.order_book)
-# draw_price_series(sim.order_book)
+draw_price_series(sim.order_book)
